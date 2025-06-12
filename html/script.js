8504e55b4663d631d5bad33187f641c59c830ed8
Tiago Miranda <t.miranda0249@gmail.com>
2025-06-12 11:56:32 -0300

    ...

---
+++ html/script.js
@@ -0,0 +1,8 @@
+function showReset() {
+    document.getElementById("formWrapper").style.transform = "translateX(-300px)";
+}
+
+function showLogin() {
+    document.getElementById("formWrapper").style.transform = "translateX(0)";
+}
+  
--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-document.getElementById("toggleSenha").addEventListener("click", function () {
-    const input = document.getElementById("senha");
-    if (input.type === "password") {
-        input.type = "text";
-    } else {
-        input.type = "password";
-    }
+const input = document.getElementById('senha');
+const toggle = document.getElementById('toggleSenha');
+
+toggle.addEventListener('click', () => {
+    const isPassword = input.type === 'password';
+    input.type = isPassword ? 'text' : 'password';
+    toggle.classList = isPassword ? 'fas fa-lock-open' : 'fas fa-lock';
 });
+
+
+
+function showReset() {
+    document.getElementById("formWrapper").style.transform = "translateX(-300px)";
+}
+
+function showLogin() {
+    document.getElementById("formWrapper").style.transform = "translateX(0)";
+}
